daa7880eb84a781dc62f6d48bc83d80f89192696
Daniel Vasquez <danielvp1987@gmail.com>
2014-08-29 12:11:57 -0500

    rutas

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def dashboard
+  end
+
+  def evalua
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root  'static_pages#dashboard'
+
+  get 'static_pages/dashboard'
+
+  get 'static_pages/evalua'
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
